93c40e1bb195620f7d6473d6a9a716bba6db1fa8
griffin4197 <gykimura@ucdavis.edu>
2019-04-17 16:24:31 -0700

    working main

--- main.c
+++ main.c
@@ -1,96 +1,94 @@
-//*****************************************************************************
-//
-// Copyright (C) 2014 Texas Instruments Incorporated - http://www.ti.com/ 
-// 
-// 
-//  Redistribution and use in source and binary forms, with or without 
-//  modification, are permitted provided that the following conditions 
-//  are met:
-//
-//    Redistributions of source code must retain the above copyright 
-//    notice, this list of conditions and the following disclaimer.
-//
-//    Redistributions in binary form must reproduce the above copyright
-//    notice, this list of conditions and the following disclaimer in the 
-//    documentation and/or other materials provided with the   
-//    distribution.
-//
-//    Neither the name of Texas Instruments Incorporated nor the names of
-//    its contributors may be used to endorse or promote products derived
-//    from this software without specific prior written permission.
-//
-//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
-//  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
-//  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-//  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
-//  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
-//  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
-//  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-//  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-//  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
-//  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
-//  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
-//*****************************************************************************
+/***************************************************
+  This is a example sketch demonstrating graphic drawing
+  capabilities of the SSD1351 library for the 1.5"
+  and 1.27" 16-bit Color OLEDs with SSD1351 driver chip
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/products/1431
+  ------> http://www.adafruit.com/products/1673
+  If you're using a 1.27" OLED, change SCREEN_HEIGHT to 96 instead of 128.
+  These displays use SPI to communicate, 4 or 5 pins are required to
+  interface
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  BSD license, all text above must be included in any redistribution
+  The Adafruit GFX Graphics core library is also required
+  https://github.com/adafruit/Adafruit-GFX-Library
+  Be sure to install it!
+ ****************************************************/
 
-//*****************************************************************************
-//
-// Application Name     - Blinky
-// Application Overview - The objective of this application is to showcase the 
-//                        GPIO control using Driverlib api calls. The LEDs 
-//                        connected to the GPIOs on the LP are used to indicate 
-//                        the GPIO output. The GPIOs are driven high-low 
-//                        periodically in order to turn on-off the LEDs.
-// Application Details  -
-// http://processors.wiki.ti.com/index.php/CC32xx_Blinky_Application
-// or
-// docs\examples\CC32xx_Blinky_Application.pdf
-//
-//*****************************************************************************
+// Screen dimensions
+#define SCREEN_WIDTH  128
+#define SCREEN_HEIGHT 128 // Change this to 96 for 1.27" OLED.
 
-//****************************************************************************
-//
-//! \addtogroup blinky
-//! @{
-//
-//****************************************************************************
+// You can use any (4 or) 5 pins
+#define SCLK_PIN 2
+#define MOSI_PIN 3
+#define DC_PIN   4
+#define CS_PIN   5
+#define RST_PIN  6
 
-// Standard includes
-#include <stdio.h>
+// Color definitions
+#define BLACK           0x0000
+#define BLUE            0x001F
+#define RED             0xF800
+#define GREEN           0x07E0
+#define CYAN            0x07FF
+#define MAGENTA         0xF81F
+#define YELLOW          0xFFE0
+#define WHITE           0xFFFF
+
+#include "Adafruit_GFX.h"
+#include "Adafruit_SSD1351.h"
+//#include <SPI.h>
+#include <string.h>
 
 // Driverlib includes
 #include "hw_types.h"
-#include "hw_ints.h"
 #include "hw_memmap.h"
 #include "hw_common_reg.h"
-#include "interrupt.h"
-#include "hw_apps_rcm.h"
-#include "prcm.h"
+#include "hw_ints.h"
+#include "spi.h"
 #include "rom.h"
 #include "rom_map.h"
-#include "gpio.h"
 #include "utils.h"
+#include "prcm.h"
 #include "uart.h"
+#include "interrupt.h"
 // Common interface includes
-#include "gpio_if.h"
 #include "uart_if.h"
 #include "pinmux.h"
-//#include "string.h"
-#define APPLICATION_VERSION     "1.1.1"
+#include "gpio.h"
+#define SPI_IF_BIT_RATE  100000
+#define TR_BUFF_SIZE     100
+#include "i2c_if.h"
+
+
 
 //*****************************************************************************
-//                          MACROS
+//                      MACRO DEFINITIONS
 //*****************************************************************************
-#define APPLICATION_VERSION  "1.1.1"
-#define APP_NAME             "UART Echo"
-#define CONSOLE              UARTA0_BASE
-#define UartGetChar()        MAP_UARTCharGet(CONSOLE)
-#define UartPutChar(c)       MAP_UARTCharPut(CONSOLE,c)
-#define MAX_STRING_LENGTH    80
+#define APPLICATION_VERSION     "1.1.1"
+#define APP_NAME                "I2C Demo"
+#define UART_PRINT              Report
+#define FOREVER                 1
+#define CONSOLE                 UARTA0_BASE
+#define FAILURE                 -1
+#define SUCCESS                 0
+#define RETERR_IF_TRUE(condition) {if(condition) return FAILURE;}
+#define RET_IF_ERR(Func)          {int iRetVal = (Func); \
+                                   if (SUCCESS != iRetVal) \
+                                     return  iRetVal;}
+
 //*****************************************************************************
 //                 GLOBAL VARIABLES -- Start
 //*****************************************************************************
-volatile int g_iCounter = 0;
+static unsigned char g_ucTxBuff[TR_BUFF_SIZE];
+static unsigned char g_ucRxBuff[TR_BUFF_SIZE];
+static unsigned char ucTxBuffNdx;
+static unsigned char ucRxBuffNdx;
+
 #if defined(ccs)
 extern void (* const g_pfnVectors[])(void);
 #endif
@@ -102,75 +100,26 @@ extern uVectorEntry __vector_table;
 //*****************************************************************************
 
 
-//*****************************************************************************
-//                      LOCAL FUNCTION PROTOTYPES                           
-//*****************************************************************************
-void LEDBlinkyRoutine();
-static void BoardInit(void);
 
-//*****************************************************************************
-//                      LOCAL FUNCTION DEFINITIONS                         
-//*****************************************************************************
 
-//*****************************************************************************
-//
-//! Configures the pins as GPIOs and peroidically toggles the lines
-//!
-//! \param None
-//! 
-//! This function  
-//!    1. Configures 3 lines connected to LEDs as GPIO
-//!    2. Sets up the GPIO pins as output
-//!    3. Periodically toggles each LED one by one by toggling the GPIO line
-//!
-//! \return None
-//
-//*****************************************************************************
-void LEDBlinkyRoutine()
-{
-    //
-    // Toggle the lines initially to turn off the LEDs.
-    // The values driven are as required by the LEDs on the LP.
-    //
-    GPIO_IF_LedOff(MCU_ALL_LED_IND);
-    while(1)
-    {
-        //
-        // Alternately toggle hi-low each of the GPIOs
-        // to switch the corresponding LED on/off.
-        //
-        MAP_UtilsDelay(8000000);
-        GPIO_IF_LedOn(MCU_RED_LED_GPIO);
-        MAP_UtilsDelay(8000000);
-        GPIO_IF_LedOff(MCU_RED_LED_GPIO);
-        MAP_UtilsDelay(8000000);
-        GPIO_IF_LedOn(MCU_ORANGE_LED_GPIO);
-        MAP_UtilsDelay(8000000);
-        GPIO_IF_LedOff(MCU_ORANGE_LED_GPIO);
-        MAP_UtilsDelay(8000000);
-        GPIO_IF_LedOn(MCU_GREEN_LED_GPIO);
-        MAP_UtilsDelay(8000000);
-        GPIO_IF_LedOff(MCU_GREEN_LED_GPIO);
-    }
+// Option 1: use any pins but a little slower
+//Adafruit_SSD1351 tft = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, CS_PIN, DC_PIN, MOSI_PIN, SCLK_PIN, RST_PIN);
 
-}
-//*****************************************************************************
-//
-//! Board Initialization & Configuration
-//!
-//! \param  None
-//!
-//! \return None
-//
-//*****************************************************************************
+// Option 2: must use the hardware SPI pins
+// (for UNO thats sclk = 13 and sid = 11) and pin 10 must be
+// an output. This is much faster - also required if you want
+// to use the microSD card (see the image drawing example)
+//Adafruit_SSD1351 tft = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI, CS_PIN, DC_PIN, RST_PIN);
+
+
 static void
 BoardInit(void)
 {
 /* In case of TI-RTOS vector table is initialize by OS itself */
 #ifndef USE_TIRTOS
-    //
-    // Set vector table base
-    //
+  //
+  // Set vector table base
+  //
 #if defined(ccs)
     MAP_IntVTableBaseSet((unsigned long)&g_pfnVectors[0]);
 #endif
@@ -178,7 +127,6 @@ BoardInit(void)
     MAP_IntVTableBaseSet((unsigned long)&__vector_table);
 #endif
 #endif
-    
     //
     // Enable Processor
     //
@@ -187,165 +135,176 @@ BoardInit(void)
 
     PRCMCC3200MCUInit();
 }
-//****************************************************************************
-//
-//! Main function
-//!
-//! \param none
-//! 
-//! This function  
-//!    1. Invokes the LEDBlinkyTask
-//!
-//! \return None.
-//
-//****************************************************************************
-void binary_value(int value){
-    MAP_UtilsDelay(8000000);
-    switch(value){
-    case 1:     GPIO_IF_LedOn(9);
-                break;
-    case 2:     GPIO_IF_LedOff(9);
-                GPIO_IF_LedOn(10);
-                break;
-    case 3:     GPIO_IF_LedOn(9);
-                break;
-    case 4:     GPIO_IF_LedOff(9);
-                GPIO_IF_LedOff(10);
-                GPIO_IF_LedOn(11);
-                break;
-    case 5:     GPIO_IF_LedOn(9);
-                break;
-    case 6:     GPIO_IF_LedOff(9);
-                GPIO_IF_LedOn(10);
-                break;
-    case 7:     GPIO_IF_LedOn(9);
-                break;
-    default:    GPIO_IF_LedOff(MCU_ALL_LED_IND);
-                break;
-    }
-}
 
 
-void poll(int blink){
-    int SW_2 = GPIOPinRead(0x40006000, 0x40);
-    int first = 0;
-    while (SW_2 > 0){
-            GPIOPinWrite(GPIOA3_BASE, 0x10, 0x10);
-            if (first == 0)
-                Message("SW2 Pressed\n\r");
-                first = first + 1;
-            if (blink == 0){
-                GPIO_IF_LedOn(9);
-                GPIO_IF_LedOn(10);
-                GPIO_IF_LedOn(11);
-                blink = 1;
-                MAP_UtilsDelay(16000000);
-            }
-            else{
-                GPIO_IF_LedOff(9);
-                GPIO_IF_LedOff(10);
-                GPIO_IF_LedOff(11);
-                blink = 0;
-                MAP_UtilsDelay(16000000);
-            }
-            SW_2 = GPIOPinRead(0x40006000, 0x40);
-        }
-    GPIOPinWrite(GPIOA3_BASE, 0x10, 0x00);
-    MAP_UtilsDelay(8000000);
-}
-int
-main()
+void main()
 {
-    //
-    // Initialize Board configurations
-    //
-    BoardInit();
-    
-    //
-    // Power on the corresponding GPIO port B for 9,10,11.
-    // Set up the GPIO lines to mode 0 (GPIO)
-    //
-    PinMuxConfig();
-    GPIO_IF_LedConfigure(LED1|LED2|LED3);
+    int iRetVal;
+       char acCmdStore[512];
 
-    GPIO_IF_LedOff(MCU_ALL_LED_IND);
-    InitTerm();
-    ClearTerm();
-    Message("*************************************************\n\r");
-    Message("\tCC3200 GPIO Application\n\r");
-    Message("*************************************************\n\n\n\r");
-    Message("****************************************************\n\r");
-    Message("\tPush SW3 to start LED binary counting\n\r");
-    Message("\tPush SW2 to blink LEDs on and off\n\r");
-    Message("****************************************************\n\r");
-    //
-    // Start the LEDBlinkyRoutine
-    //
-    //GPIO_IF_LedOn(MCU_GREEN_LED_GPIO);
-    char * word = "check is: ";
-    char number[5];
-    int SW_2 = 0;
-    int SW_3 = 0;
-    int blink = 0;
-    int value = 0;
-    GPIOPinWrite(GPIOA3_BASE, 0x10, 0x00);
-    while(1){
-    int check = 0;
+       //
+       // Initialize board configurations
+       //
+       BoardInit();
 
-    //check 32 is SW3 ON
-    //Message(word);
-    //sprintf(number, "%d", check);
-    //Message(number);
-    MAP_UtilsDelay(8000000);
-    int value = 0;
-    if (SW_3 > 0){
-        Message("SW3 Pressed\n\r");
-        while(value < 8 && SW_2 == 0){
-            GPIOPinWrite(GPIOA3_BASE, 0x10, 0x00);
-            binary_value(value++);
-            SW_2 = GPIOPinRead(0x40006000, 0x40);
-        }
-        SW_3 = 0;
-    }
-    if (SW_2 > 0){
-        Message("SW2 Pressed\n\r");
+       //
+       // Configure the pinmux settings for the peripherals exercised
+       //
+       PinMuxConfig();
 
-        while (SW_3 == 0){
-            GPIOPinWrite(GPIOA3_BASE, 0x10, 0x10);
-        if (blink == 0){
-        GPIO_IF_LedOn(9);
-        GPIO_IF_LedOn(10);
-        GPIO_IF_LedOn(11);
-        blink = 1;
-        MAP_UtilsDelay(8000000);
-        }
-        else{
-            GPIO_IF_LedOff(9);
-            GPIO_IF_LedOff(10);
-            GPIO_IF_LedOff(11);
-            blink = 0;
-            MAP_UtilsDelay(8000000);
-        }
-        SW_3= GPIOPinRead(0x40005000, 0x20);
-        }
-        SW_2 = 0;
-    }
-    else{
-        SW_2 = GPIOPinRead(0x40006000, 0x40);
-        //check 64 is SW2 ON
-        SW_3= GPIOPinRead(0x40005000, 0x20);
-        GPIO_IF_LedOff(9);
-        GPIO_IF_LedOff(10);
-        GPIO_IF_LedOff(11);
-    }
-    //LEDBlinkyRoutine();
-    }
-    return 0;
+       //
+       // Configuring UART
+       //
+       InitTerm();
+
+       //
+       // Clearing the Terminal.
+       //
+       ClearTerm();
+
+       //
+       // I2C Init
+
+
+    //
+    // Enable the SPI module clock
+    //
+
+    MAP_PRCMPeripheralClkEnable(PRCM_GSPI,PRCM_RUN_MODE_CLK);
+
+
+    MAP_PRCMPeripheralReset(PRCM_GSPI);
+
+
+//Start of MasterMain()
+    //unsigned long ulUserData;
+    //unsigned long ulDummy;
+
+    //
+    // Initialize the message
+    //
+    //memcpy(g_ucTxBuff,MASTER_MSG,sizeof(MASTER_MSG));
+
+    //
+    // Set Tx buffer index
+    //
+   // ucTxBuffNdx = 0;
+    //ucRxBuffNdx = 0;
+
+    //
+    // Reset SPI
+    //
+    MAP_SPIReset(GSPI_BASE);
+
+    //
+    // Configure SPI interface
+    //
+    MAP_SPIConfigSetExpClk(GSPI_BASE,MAP_PRCMPeripheralClockGet(PRCM_GSPI),
+                     SPI_IF_BIT_RATE,SPI_MODE_MASTER,SPI_SUB_MODE_0,
+                     (SPI_SW_CTRL_CS |
+                     SPI_4PIN_MODE |
+                     SPI_TURBO_OFF |
+                     SPI_CS_ACTIVEHIGH |
+                     SPI_WL_8));
+
+    //
+    // Enable SPI for communication
+    //
+    MAP_SPIEnable(GSPI_BASE);
+    // Reset the peripheral
+    //
+    //Set reset low to initialize chip
+    //GPIOPinWrite(GPIOA3_BASE, 0x10, 0x00); //P18
+
+    //Chip select set to high initially
+    GPIOPinWrite(GPIOA2_BASE, 0x40, 0x40);
+
+    Adafruit_Init();
+
+
+    fillScreen(RED);
+    int x = 63;
+    int y = 63;
+    fillCircle(x,y, 4, BLUE);
+    //
+    I2C_IF_Open(I2C_MASTER_MODE_FST);
+    unsigned char ucDevAddr, ucRegOffset, ucRdLen;
+      unsigned char aucRdDataBuf[6];
+
+
+      //
+      // Get the device address
+      //
+
+      ucDevAddr = 0x18;
+      //
+      // Get the register offset address
+      //
+
+      ucRegOffset = 0x2;
+
+      //
+      // Get the length of data to be read
+      ucRdLen = 6;
+      //RETERR_IF_TRUE(ucLen > sizeof(aucDataBuf));
+
+      //
+      // Write the register address to be read from.
+      // Stop bit implicitly assumed to be 0.
+      //
+      char x_acc;
+      char y_acc;
+      int old_x = 0;
+      int old_y = 0;
+      int radius = 6;
+      while(1){
+          fillCircle(old_x, old_y, radius, RED);
+      RET_IF_ERR(I2C_IF_Write(ucDevAddr,&ucRegOffset,1,0));
+
+      //
+      // Read the specified length of data
+      //
+      RET_IF_ERR(I2C_IF_Read(ucDevAddr, &aucRdDataBuf[0], ucRdLen));
+
+      //initial position of ball
+      x_acc = aucRdDataBuf[1];
+      y_acc = aucRdDataBuf[3];
+      //range of x_acc and y_acc is 0-255
+      //8-bit two's complement
+      //two's complement so the first bit is positive or negative
+      fillCircle(y, x , radius, BLUE);
+      old_x = y;
+      old_y = x;
+      if (x_acc > 127){
+          x_acc = x_acc ^ 0xFF;
+          x_acc = x_acc + 1;
+
+      }
+      x_acc = -x_acc;
+      if (y_acc > 127){
+          y_acc = y_acc ^ 0xFF;
+          y_acc = y_acc + 1;
+
+      }
+      y_acc = -y_acc;
+      //the circle should go from left to right in two seconds
+      //We do not entirely know entire pipeline length
+      //Assume updates 10 times a second or something
+      //0-128 in 2 seconds
+      //64 pixels per second
+      //10 updates per second
+      //at max speed 127/20
+      //6.35 pixels per update
+      int speed = 200;
+      x = x - x_acc/speed;
+      y = y - y_acc/speed;
+      if (x < 3)
+          x = 3;
+      if (y < 3)
+          y = 3;
+      if (x > 124)
+          x = 124;
+      if (y > 124)
+          y = 124;
+      }
 }
-
-//*****************************************************************************
-//
-// Close the Doxygen group.
-//! @}
-//
-//*****************************************************************************
